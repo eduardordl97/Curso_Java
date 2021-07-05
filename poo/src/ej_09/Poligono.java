9a7263d772202bb3e14d193ec515e0b4a8adeeca
eduardordl97 <erdl5259@gmail.com>
2021-07-05 16:12:59 -0500

    ejercicio final poo

---
+++ poo/src/ej_09/Poligono.java
@@ -0,0 +1,26 @@
+package ej_09;
+
+public abstract class Poligono {
+	
+	protected int nLados;
+	
+	public Poligono(int nLados) {
+		this.nLados = nLados;
+	}
+
+	public int getnLados() {
+		return nLados;
+	}
+
+	@Override
+	public String toString() {
+		return "Numero de lados: " + nLados;
+	}
+	
+	public abstract double area();
+
+	
+	
+	
+
+}
